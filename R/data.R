2a96f7f92188c93023599f1fb72b5774cbe9b4c8
Ricardo Lemos <rtl00@yahoo.com>
2016-07-31 19:03:31 -0700

    typos in data descriptor

--- R/data.R
+++ R/data.R
@@ -1,8 +1,8 @@
-#' Log-catches-at-age of Gulf menhaden in the U.S. Gulf of Mexico, 1964-2004 
+#' Log-catches-at-age of Gulf menhaden in the U.S. Gulf of Mexico, 1964-2004.
 #'
 #' A data set containing Y = log(X + 0.01), where X is the estimated annual 
 #' catch-at-age, in million fish, of Brevoortia patronus, from the reduction
-#' fishery. Derived from Table 2 of Vaughan et al., 2007, Fish. Res. 83, 263-275
+#' fishery. Derived from Table 2 of Vaughan et al., 2007, Fish. Res. 83, 263-275.
 #'
 #' @format A data frame with 41 rows and 6 columns
 #' \describe{
@@ -14,11 +14,11 @@
 "Y"
 
 #' Nominal fishing effort of the gulf menhaden reduction fishery in the U.S.
-#' Gulf of Mexico, 1964-2004  
+#' Gulf of Mexico, 1964-2004. 
 #'
 #' A data set containing nominal effort, in 1000 vessel-tonne-weeks, associated
-#' with the US menhaden reduction fishery in the Gulf of Mexico
-#' Extracted from Table 2 of Vaughan et al., 2007, Fish. Res. 83, 263-275
+#' with the US menhaden reduction fishery in the Gulf of Mexico.
+#' Extracted from Table 2 of Vaughan et al., 2007, Fish. Res. 83, 263-275.
 #'
 #' @format A data frame with 41 rows and 1 columns
 #' \describe{
